4da53795e44acf8c93a58cf8d6b9a3cf5675eda4
Santi46M <santimendoza499@gmail.com>
2024-03-29 14:22:02 -0300

    ??

--- Laboratorio_0/src/DTInfoEstudiante.cpp
+++ Laboratorio_0/src/DTInfoEstudiante.cpp
@@ -1,7 +1,17 @@
 #include "../inc/DTInfoEstudiante.h"
 
-DTInfoEstudiante::DTInfoEstudiante(int ci, int identificadorInfo, string NombreEst){
+DTInfoEstudiante::DTInfoEstudiante(int ci, int identificadorInfo, string NombreEst)
+{
 	this->ci = ci;
 	this->identificadorInfo = identificadorInfo;
 	this->NombreEst = NombreEst;
-}
+
+	DTInfoEstudiante(int ci, int identificadorInfo, string NombreEst)
+		: ci(ci), identificadorInfo(identificadorInfo), NombreEst(NombreEst) {}
+
+	friend ostream &operator<<(ostream &os, const DTInfoEstudiante &estudiante)
+	{
+		os << estudiante.ci << ", " << estudiante.identificadorInfo << ", " << estudiante.NombreEst;
+		return os;
+	}
+};
