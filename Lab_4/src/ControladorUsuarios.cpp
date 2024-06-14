134feedc45ee32d3a0f3454c40ea37eb43f48070
Santi46M <santimendoza499@gmail.com>
2024-06-14 19:44:34 -0300

    ?

--- Lab_4/src/ControladorUsuarios.cpp
+++ Lab_4/src/ControladorUsuarios.cpp
@@ -29,7 +29,7 @@ bool ControladorUsuarios::altaVendedor(string username, string password, DTFecha
 
 set<string> ControladorUsuarios::listaDeUsuarios() {
 	set<string> listaUsuarios;
-	for (vector<Usuario*>::iterator it = Usuario.begin(); it != Usuario.end(); ++it) {
+	for (vector<Usuario*>::iterator it = Usuarios.begin(); it != Usuarios.end(); ++it) {
 		listaUsuarios.insert((*it)->getNickname());
 	}
 	return listaUsuarios;
