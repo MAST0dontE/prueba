c31423a37701a520698cb887a63db316db09acf4
Ori <orianaaguilar28@gmail.com>
2024-06-13 21:48:57 -0300

    constructor

--- Lab_4/src/DTInfoProducto.cpp
+++ Lab_4/src/DTInfoProducto.cpp
@@ -1,5 +1,4 @@
 #include "DTInfoProducto.h"
 
-DTInfoProducto::DTInfoProducto(int codigo, string nombre, float precio, int cantStock, string descripcion,  ECategoria categoria, string vendedor){
-
-};
+DTInfoProducto::DTInfoProducto(int codigo, string nombre, float precio, int cantStock, string descripcion,  ECategoria categoria, string vendedor): codigo(codigo), nombre(nombre), precio(precio), cantStock(cantStock), descripcion(descripcion), categoria(categoria), vendedor(vendedor)
+{}
